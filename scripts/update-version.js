87088eed90f6e99c435e4ea7931bc8bed061a750
Konnor Rogers <konnor5456@gmail.com>
2024-08-18 04:24:10 -0400

    update version scripts

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -6,7 +6,8 @@ const __filename = url.fileURLToPath(new URL(import.meta.url));
 
 const filepath = path.relative(process.cwd(), __filename)
 
-const version = process.env.npm_package_version
+const version = process.env.PACKAGE_VERSION
+console.log("VERSION: ", version)
 if (!version) {
   console.error(`"${filepath}" must be run via NPM's runner. Use: "npm run update-version" for this to work properly.`)
   process.exit(1)
